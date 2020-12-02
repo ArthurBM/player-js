0ee9edf25ed7d82a29d22f764a2703e8f2e755ac
Arthur Brito <arthurmedeiros32@gmail.com>
2020-12-01 21:55:18 -0300

    some more changes

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -1,9 +1,21 @@
-// import musicNames from "./mp3data";
-// import elements from "./elements";
+import musicNames from "./mp3data";
+import elements from "./elements";
 
 class Player {
     constructor({ playerId }){
-      elements.get.call(this);
+      // elements.get.call(this);
+      this.playerElement = document.querySelector(`#${playerId}`);
+      this.controls = this.playerElement.querySelector('.controls');
+      this.progressBar = this.playerElement.querySelector('.progress-bar');
+      this.playBtn = this.controls.querySelector('.play-pause');
+      this.forwardBtn = this.controls.querySelector('.forward');
+      this.backwardBtn = this.controls.querySelector('.backward');
+      this.audioElement = this.playerElement.querySelector('audio');
+      this.imgPlayPause = this.playBtn.querySelector('img');
+      this.ballProgressBar = document.querySelector('.ball-progress-bar');
+      this.timeStamp = document.querySelector('.time-stamp');
+      this.progressBarWrapper = document.querySelector('.progress-bar-wrapper');
+      
   
       this.handleEventListeners();
     }
